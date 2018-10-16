42fd503f32e9cc0815f2c4f8db2e8a95911488bf
Anthony Todek <tonytodek@yahoo.com>
2018-10-16 08:52:46 -0400

    initial commit

---
+++ Capstone_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Capstone_1
+{
+    class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a word or phrase to translate to PigLatin:");
+            string userInput = Console.ReadLine();
+            var pigLatin = GetSentenceInPigLatin(userInput);
+            Console.WriteLine(pigLatin);
+            Console.ReadLine();
+        }
+
+        private static string GetSentenceInPigLatin(string userInput)
+        {
+            const string vowels = "AEIOUaeiou";
+            var returnSentence = "";
+            foreach (var word in userInput.Split())
+            {
+                var firstLetter = word.Substring(0, 1);
+                var restOfWord = word.Substring(1, word.Length - 1);
+                var currentLetter = vowels.IndexOf(firstLetter, StringComparison.Ordinal);
+
+                if (currentLetter == -1)
+                {
+                    returnSentence += restOfWord + firstLetter + "ay ";
+                }
+                else
+                {
+                    returnSentence += word + "way ";
+                }
+            }
+            return returnSentence;
+        }
+    }
+}
